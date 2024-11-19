b09a77c82c7258a425de24437ce0956c0c6c9bc7
AasifNazir786 <aasif.n.s786@gmail.com>
2024-11-19 11:15:49 +0530

    modified main.go file

--- main.go
+++ main.go
@@ -10,6 +10,7 @@ func main() {
 	fmt.Println("Hello World")
 
 	service.InitDB()
+	defer service.CloseDB()
 	//create an Object of Address struct
 	address := types.Address{
 		Country: "India",
@@ -42,60 +43,58 @@ func main() {
 
 	fmt.Println("Add User to in-memory database.................")
 	fmt.Println("===============================================================")
-	printUser := service.AddUser(user1)
-	fmt.Println(printUser)
-	printUser1 := service.AddUser(user2)
-	fmt.Println(printUser1)
+	service.AddUser(user1)
+	service.AddUser(user2)
 	fmt.Println()
 
 	fmt.Println("Retrieve all Users..............................")
 	fmt.Println("===============================================================")
-	users := service.GetUsers()
+	users, _ := service.GetAllUsers()
 	for _, val := range users {
 		fmt.Println(val)
 	}
 	fmt.Println()
 
-	fmt.Println("Retrieve data from database using id...................")
-	fmt.Println("===============================================================")
-	id := 1
-	fmt.Println(service.GetUserById(id))
-	fmt.Println()
+	// fmt.Println("Retrieve data from database using id...................")
+	// fmt.Println("===============================================================")
+	// id := 1
+	// fmt.Println(service.GetUserById(id))
+	// fmt.Println()
 
-	fmt.Println("Update User at a specific id.....................")
-	fmt.Println("===============================================================")
-	updateUser := types.User{
-		UserId:    101,
-		FirstName: "Aabid Hamid",
-		LastName:  "Shah",
-		UserEmail: "aabid786@gmail.com",
-		UserAddress: types.Address{
-			Country: "India",
-			State:   "Jammu And Kashmir",
-			City:    "Srinagar",
-			ZipCode: 192301,
-		},
-	}
-	fmt.Println(service.UpdateUser(1, updateUser))
-	fmt.Println()
+	// fmt.Println("Update User at a specific id.....................")
+	// fmt.Println("===============================================================")
+	// updateUser := types.User{
+	// 	UserId:    101,
+	// 	FirstName: "Aabid Hamid",
+	// 	LastName:  "Shah",
+	// 	UserEmail: "aabid786@gmail.com",
+	// 	UserAddress: types.Address{
+	// 		Country: "India",
+	// 		State:   "Jammu And Kashmir",
+	// 		City:    "Srinagar",
+	// 		ZipCode: 192301,
+	// 	},
+	// }
+	// fmt.Println(service.UpdateUser(1, updateUser))
+	// fmt.Println()
 
-	fmt.Println("Retrieve all Users after updation..............................")
-	fmt.Println("===============================================================")
-	users1 := service.GetUsers()
-	for _, val := range users1 {
-		fmt.Println(val)
-	}
-	fmt.Println()
+	// fmt.Println("Retrieve all Users after updation..............................")
+	// fmt.Println("===============================================================")
+	// users1, _ := service.GetAllUsers();
+	// for _, val := range users1 {
+	// 	fmt.Println(val)
+	// }
+	// fmt.Println()
 
-	// Delete user from in-memory database
-	service.DeleteUser(1)
+	// // Delete user from in-memory database
+	// service.DeleteUser(1)
 
-	fmt.Println("Retrieve all Users after deletion..............................")
-	fmt.Println("===============================================================")
-	users2 := service.GetUsers()
-	for _, val := range users2 {
-		fmt.Println(val)
-	}
-	fmt.Println()
+	// fmt.Println("Retrieve all Users after deletion..............................")
+	// fmt.Println("===============================================================")
+	// users2, _ := service.GetAllUsers()
+	// for _, val := range users2 {
+	// 	fmt.Println(val)
+	// }
+	// fmt.Println()
 
 }
